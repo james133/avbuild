33c860e9497a59dfe75b5503784e117c55457694
james133 <james133@126.com>
2021-01-01 14:10:53 +0800

    Update buildx264.sh

--- deps/buildx264.sh
+++ deps/buildx264.sh
@@ -3,8 +3,8 @@ parent_dir=$(dirname $(pwd))
 
 tar jxvf x264-master.tar.bz2
 cd x264-master
-./configure --prefix=${parent_dir}/release --enable-static
+./configure --prefix=${parent_dir}/release --enable-static --enable-pic
 make && make install
 
-./configure --prefix=/usr --enable-static
+./configure --prefix=/usr --enable-static --enable-pic
 make && make install

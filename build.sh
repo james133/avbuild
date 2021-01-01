6d6fe072d0223bc4816a089d3a3a685d06680cad
james133 <james133@126.com>
2021-01-01 11:53:42 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ parent_dir=$(dirname $(pwd))
 if [ -e ./ffmpeg-4.3.1.tar.bz2 ]; then
   /bin/rm -rf ffmpeg-4.3.1
   /bin/rm -rf *sdk-*
+  /bin/rm -rf build_sdk-*
   tar jxvf ffmpeg-4.3.1.tar.bz2
 else
    echo "please downlaod ffmpeg-4.3.1.tar.bz2 http://www.ffmpeg.org/download.html"

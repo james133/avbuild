3cdfce855c2e33f1c6e07f7c5bed7f9599ce44e5
james133 <james133@126.com>
2021-01-01 12:19:23 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,5 +10,6 @@ else
    echo "please downlaod ffmpeg-4.3.1.tar.bz2 http://www.ffmpeg.org/download.html"
    exit
 fi
+export PKG_CONFIG_PATH=${cur_dir}/lib/pkgconfig
 export FFSRC=${cur_dir}/ffmpeg-4.3.1
 ./avbuild.sh

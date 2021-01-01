af9cc58e49bf7a37d52a15405ad3d96c85ba7098
james133 <james133@126.com>
2021-01-01 13:37:54 +0800

    调试fdk-aac编译问题

--- deps/buildfdk-aac.sh
+++ deps/buildfdk-aac.sh
@@ -3,5 +3,5 @@ parent_dir=$(dirname $(pwd))
 
 tar zxvf fdk-aac-0.1.6.tar.gz
 cd fdk-aac-0.1.6
-./configure --prefix=${parent_dir}/release  --enable-static --disable-shared
+./configure --prefix=${parent_dir}/release  --enable-static --enable-shared
 make && make install
